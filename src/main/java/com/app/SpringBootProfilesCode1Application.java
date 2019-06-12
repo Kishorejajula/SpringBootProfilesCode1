822e932ad63b1edcd84105f9bd3680a025a534d7
I'M THE DELL <I'M THE DELL@192.168.0.110>
2019-06-12 13:48:44 +0530

    changed

---
+++ src/main/java/com/app/SpringBootProfilesCode1Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfilesCode1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfilesCode1Application.class, args);
+	}
+
+}
